d40f5832fb22dba74c6d9cff26b2f4d1ef3924d7
gdi2290 <github@gdi2290.com>
2016-05-20 17:44:43 -0700

    chore: typings.d.ts

---
+++ src/beta-17/typings.d.ts
@@ -0,0 +1,23 @@
+declare module "angular2/core" {
+  export * from '@angular/core';
+}
+
+declare module "angular2/http" {
+  export * from '@angular/http';
+}
+
+declare module 'angular2/platform/browser' {
+  export * from '@angular/platform-browser'
+}
+
+declare module 'angular2/testing' {
+  export * from '@angular/core/testing';
+}
+
+declare module 'angular2/router' {
+  export * from '@angular/router-deprecated';
+}
+
+declare module 'angular2/http/testing' {
+  export * from '@angular/http/testing';
+}
